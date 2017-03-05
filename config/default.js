a1479a22d99022721c864cc5bc1289b04030fda1
银马座 <2223673415@qq.com>
2017-03-05 15:33:57 +0800

    Update default.js

--- config/default.js
+++ config/default.js
@@ -1,9 +1,11 @@
 module.exports = {
-  port: 3000,
+  //port: 3000,
   session: {
     secret: 'myblog',
     key: 'myblog',
     maxAge: 2592000000
   },
-  mongodb: 'mongodb://localhost:27017/myblog'
+  //mongodb: 'mongodb://localhost:27017/myblog'
+  port: 80,
+  mongodb: 'mongodb://myblog:myblog@ds119020.mlab.com:19020/myblog'
 };
